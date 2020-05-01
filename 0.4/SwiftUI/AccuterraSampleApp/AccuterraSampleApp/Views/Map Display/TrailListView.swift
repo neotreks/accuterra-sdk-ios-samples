154f2f8ec8152bdeb5c29a2ca237a367b2b0facd
Brian Elliott <brian@neotreks.com>
2020-04-30 22:43:48 -0600

    Cleanup and update gitignore

--- 0.4/SwiftUI/AccuterraSampleApp/AccuterraSampleApp/Views/Map Display/TrailListView.swift
+++ 0.4/SwiftUI/AccuterraSampleApp/AccuterraSampleApp/Views/Map Display/TrailListView.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 NeoTreks, Inc. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+import AccuTerraSDK
+import Mapbox
+import Combine
+
+struct TrailListView: View {
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    var trails: Array<TrailBasicInfo>
+
+    var body: some View {
+        List {
+            ForEach(trails.indices, id: \.self){ idx in
+                Text(self.trails[idx].name)
+            }
+        }
+        .navigationBarTitle(Text("Trail List"), displayMode: .inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: Button(action : {
+                self.mode.wrappedValue.dismiss()
+            }){
+                Image(systemName: "arrow.left")
+            })
+
+    }
+    
+}
