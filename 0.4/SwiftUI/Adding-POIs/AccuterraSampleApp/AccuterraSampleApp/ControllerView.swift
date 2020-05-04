0e8f0a753f805b4a5fc69f1a9ac8893ff1fbb50b
Brian Elliott <brian@neotreks.com>
2020-05-03 21:08:39 -0600

    Adding POIs completed

---
+++ 0.4/SwiftUI/Adding-POIs/AccuterraSampleApp/AccuterraSampleApp/ControllerView.swift
@@ -0,0 +1,27 @@
+//
+//  ControllerView.swift
+//  AccuterraSampleApp
+//
+//  Created by Brian Elliott on 5/3/20.
+//  Copyright © 2020 NeoTreks, Inc. All rights reserved.
+//
+import SwiftUI
+
+struct ControllerView : View {
+    
+    @ObservedObject var viewRouter: ViewRouter
+    
+    var body: some View {
+            VStack {
+                if viewRouter.currentPage == "download" {
+                    DownloadView(viewRouter: viewRouter)
+                } else if viewRouter.currentPage == "home" {
+                    NavigationView {
+                        // HomeView(viewRouter: viewRouter)
+                        HomeView(selectedTrailId: 0)
+                        .navigationBarTitle(Text("AccuTerra SDK SwiftUI Samples"), displayMode: .inline)
+                    }
+                }
+            }
+    }
+}
---
+++ 0.4/SwiftUI/Adding-POIs/AccuterraSampleApp/AccuterraSampleApp/HomeView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  AccuterraSampleApp
+//
+//  Created by Brian Elliott on 5/3/20.
+//  Copyright © 2020 NeoTreks, Inc. All rights reserved.
+//
+
+import SwiftUI
+import AccuTerraSDK
+import Mapbox
+
+struct HomeView: View {
+    
+    // @ObservedObject var viewRouter: ViewRouter
+    @State var selectedTrailId:Int64
+
+    
+    var body: some View {
+        VStack() {
+            MapView(selectedTrailId: $selectedTrailId)
+            Spacer()
+            HStack(spacing: 30) {
+                if selectedTrailId == 0 {
+                    Text("Picked Trail ID: N/A")
+                }
+                else {
+                    Text("Picked Trail ID: \(selectedTrailId)")
+                }
+                NavigationLink(destination: DetailView(trailId:selectedTrailId)) {
+                    Text("Trail Details")
+                }
+            }
+            .padding()
+        }
+    }
+}
+
